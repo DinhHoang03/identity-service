0544820a80ae4535e811040b8784f44c9c1a9bbb
JavHoang03 <dinhlol1012@gmail.com>
2024-12-07 15:56:27 +0700

    Commit

--- src/main/java/com/demo/identity_service/mapper/UserMapper.java
+++ src/main/java/com/demo/identity_service/mapper/UserMapper.java
@@ -10,6 +10,8 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserCreationRequest request);
+
+    @Mapping(source = "firstName", target = "lastName")
     UserResponse toUserResponse(User user);
     void updateUser(@MappingTarget User user, UserUpdateRequest request);
 }
